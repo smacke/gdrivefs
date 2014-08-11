d746e13ef84d4038e3a100fb04ed9d14b1db5fb8
Stephen Macke <smackee618@gmail.com>
2014-08-10 23:27:26 -0700

    hacky mount integration, sort of

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT=$(basename $0)
+if [ $# -lt 1 ]; then
+    echo "Usage: $SCRIPT [VERSION]"
+fi
+if [ ! -d "packaging" ]; then
+    echo "Please run from the project root directory"
+fi
+VERSION=$1
+WORKDIR=$(mktemp -d)
+DEBDIR=$WORKDIR/debian
+cp -r packaging/debian $WORKDIR
+ant -f packaging/package.xml
+mv gdrivefs.jar $DEBDIR/usr/lib/gdrivefs-"$VERSION".jar
+find $DEBDIR -type f | xargs sed -i "s/__PACKAGE_VERSION/$VERSION/g"
+pushd $WORKDIR
+dpkg --build debian
+popd
+mv $WORKDIR/debian.deb gdrivefs-"$VERSION".deb
+rm -r $WORKDIR
